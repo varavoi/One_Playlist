e9cad66ca94b839bdc033fff288b39c49cb21a04
varavoi <institut93@gmail.com>
2024-10-01 14:48:09 +0300

    Прописал логику для добавления песен из сайта drivemusic.me

---
+++ JS/Inject2.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+$("div[class='music-popular__item']").css("background","red");
+
+$("div[class='music-popular__item']").html((index, oldHtml)=>{
+       return oldHtml + "<button class = 'addBtn' title='Добавить в свой плейлист'></button>"
+    })
+
+   //alert(songName+" " + artistName)
+window.globalJsonArray = [];
+
+function addToJsonArray(obj){
+    chrome.storage.local.get(['jsonArray'],(result)=>{
+        const jsonArray = result.jsonArray || [];
+        const jsonString = JSON.stringify(obj)
+        jsonArray.push(jsonString)
+   
+    chrome.storage.local.set({jsonArray},()=>{
+        console.log("Добавлен в ", jsonString)
+        })
+    })
+}
+function getInfoFromClick(e){
+    let songName = e.closest(".music-popular__item").find(".popular-play-name>a").html()
+    let artistName = e.closest(".music-popular__item").find(".popular-play-composition>a").html()
+    let musicURL=e.closest(".music-popular__item").find(".btn_player>button").attr("data-url")
+    let songTime = e.closest(".track-b").find("div[class = 'track-b-duration']").html()
+    let a = songName+" "+artistName+" "+songTime
+    let obj = {}
+    obj.songName= songName
+    obj.artistName= artistName
+    obj.songTime= songTime
+    obj.musicURL = musicURL
+    return obj
+}
+function getCurrentAudioLink() {
+    chrome.storage.local.get(['currentAudioLink'], function(result) {
+        if (result.currentAudioLink) {
+            console.log('Текущая аудиосссылка из хранилища:', result.currentAudioLink);
+            // Здесь вы можете взаимодействовать с этой ссылкой как хотите
+            let currentAudioLink = result.currentAudioLink;
+            // Например, использовать текущую ссылку для дальнейших действий
+        } else {
+            console.log('Аудиосссылка не найдена.');
+        }
+    });
+}
+
+
+$(".addBtn").on("click",function(e){
+    let songObj =  getInfoFromClick($(this))
+    addToJsonArray(songObj)
+    
+    //alert(json)
+})
+$(".track-b-play").on("click",function(e){
+    getCurrentAudioLink();
+    // Вызов функции получения аудиоссылки
+})
+
+})

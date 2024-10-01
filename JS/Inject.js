ff587c9b746c25a787ecefd56e09bce411f5d7ce
varavoi <institut93@gmail.com>
2024-10-01 14:25:00 +0300

    При нажатии на кнопку, записываем название песни, имя артиста и ссылку песни во встроенное хранилище Google Chrome

--- JS/Inject.js
+++ JS/Inject.js
@@ -5,5 +5,35 @@ $("div[class='track-b']").html((index, oldHtml)=>{
        return oldHtml + "<button class = 'addBtn' title='Добавить в свой плейлист'></button>"
     })
 
+    window.globalJsonArray = [];
 
+    function addToJsonArray(obj){
+        chrome.storage.local.get(['jsonArray'],(result)=>{
+            const jsonArray = result.jsonArray || [];
+            const jsonString = JSON.stringify(obj)
+            jsonArray.push(jsonString)
+       
+        chrome.storage.local.set({jsonArray},()=>{
+            console.log("Добавлен в ", jsonString)
+            })
+        })
+    }
+    function getInfoFromClick(e){
+        let songName = e.closest(".track-b").find(".track-b-wrap>a[class='name']").html()
+        let artistName = e.closest(".track-b").find(".track-b-wrap>a[class='singer']").html()
+        let songTime = e.closest(".track-b").find("div[class = 'track-b-duration']").html()
+        let a = songName+" "+artistName+" "+songTime
+        let obj = {}
+        obj.songName= songName
+        obj.artistName= artistName
+        obj.songTime= songTime
+        return obj
+    }
+    
+    $(".addBtn").on("click",function(e){
+        let songObj =  getInfoFromClick($(this))
+        addToJsonArray(songObj)
+        //alert(json)
+    })
+    
 })

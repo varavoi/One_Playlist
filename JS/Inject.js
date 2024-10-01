a8e8c02b8e0722da57cd0a971c7bf87bab053ec3
varavoi <institut93@gmail.com>
2024-10-01 13:58:48 +0300

    В manifest.json в content_scripts прописал файлы работы с сайтом tetamuz.online

---
+++ JS/Inject.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+$("div[class='track-b']").css("background","red");
+
+$("div[class='track-b']").html((index, oldHtml)=>{
+       return oldHtml + "<button class = 'addBtn' title='Добавить в свой плейлист'></button>"
+    })
+
+
+})
